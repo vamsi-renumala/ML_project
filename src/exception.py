c5a959e083b27d07a08322cdc4df64e77c92e761
vamsirenumala <vamsirenumala000@gmail.com>
2024-10-22 19:06:30 +0530

    Nothing

--- src/exception.py
+++ src/exception.py
@@ -15,4 +15,4 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
-        return self.error_message
+        return self.error_message 
